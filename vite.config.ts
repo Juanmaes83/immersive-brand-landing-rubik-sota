2dd9dab0001b431d48a7641659e1d77366ec4bef
Juanmaes83 <juanmaes83@gmail.com>
2026-04-08 12:05:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/immersive-brand-landing-rubik-sota/',
+  base: process.env.GITHUB_ACTIONS ? '/immersive-brand-landing-rubik-sota/' : '/',
   plugins: [react(), tailwindcss()],
 })
