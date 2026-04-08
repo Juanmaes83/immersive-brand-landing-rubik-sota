9538a3c4c8631a6d0023c0081e2ccc10e6c45f96
Juanmaes83 <juanmaes83@gmail.com>
2026-04-08 11:48:32 +0200

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/immersive-brand-landing-rubik-sota/',
   plugins: [react(), tailwindcss()],
 })
